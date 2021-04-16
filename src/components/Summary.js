b24188651fe459117797a682b5709f9379990626
Bayard Eton <beton@bu.edu>
2021-04-16 13:33:34 -0400

    some styling

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -8,10 +8,10 @@ export default class Summary extends Component {
                 <h2 className='abt-me'>About Me</h2>
                 <span className='abt-me-p-wrap'>
                     <p className='abt-me-p'>
-                        This is a paragraph about me.
+                        Bayard is graduating this May with a B.S. in Computer Engineering from Boston University. He has 1 year of experience as a full-stack software engineer.
                     </p>
                     <p className='abt-me-p'>
-                        This is another paragraph about me.
+                        He is planning on pursuing a higher degree in the next 2-3 years and is excited to start a software company within the next 5.
                     </p>
                 </span>
                 <Skills />
